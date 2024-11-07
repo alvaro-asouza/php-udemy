86e00d7b37b264694e7822df6412d71b12dd87be
alvaro-asouza <alvaroalves928@gmail.com>
2024-11-07 23:41:23 +0000

    adicionando arquivos de conteudo php

---
+++ comentarios.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+
+    <?php 
+        // Comentario de uma linha
+        echo 'Comando echo <br/>';
+        # Comentario de uma linha
+        echo print('Comando print <hr/>');
+
+        /*
+        comentario de multiplas linhas
+        */
+    ?>
+    
+</body>
+</html>
---
+++ echo_print.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <?php 
+        echo 'Comando echo <br/>';
+
+        echo print('Comando print <hr/>');
+    ?>
+</body>
+</html>
